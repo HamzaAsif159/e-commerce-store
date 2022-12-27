295db2eba83f2419ac3321ec6973e1bb43a65d41
Hamza Asif <hmziiiraj@gmail.com>
2022-12-27 20:11:54 +0500

    Added count for cart items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 export default function Header() {
+  const cartItemsCount = useSelector(
+    (state) => state.allcartItems.cartItems.length
+  )
   return (
     <nav className="bg-blue-800 h-16 flex items-center justify-between px-8">
       <h2 className=" text-white text-2xl font-bold cursor-pointer uppercase">
@@ -11,8 +15,11 @@ export default function Header() {
         <img
           src="./cart.svg"
           alt="cart-icon"
-          className="h-8 text-white cursor-pointer"
+          className="h-8 inline text-white cursor-pointer"
         />
+        <div className="text-white text-3xl font-semibold  inline ml-2">
+          {cartItemsCount}
+        </div>
       </Link>
     </nav>
   )
